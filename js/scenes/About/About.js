d154bfe36fd5443d80f4353aa4c4f2e3edd05b79
Late Worm <travis.rempel@gmail.com>
2018-02-26 19:04:24 -0800

    Initialize repository.

---
+++ js/scenes/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+const About = () => (
+  <View>
+    <Text>About Scene. How about that?</Text>
+  </View>
+);
+
+export default About;
---
+++ js/scenes/About/AboutContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import About from "./About";
+
+class AboutContainer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <About />;
+  }
+}
+
+export default AboutContainer;
