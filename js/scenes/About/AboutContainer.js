08155ed7e18ccfd6c0abfa175f193c423af4cdd5
Late Worm <travis.rempel@gmail.com>
2018-02-26 21:04:17 -0800

    Fetch and display Code of Conduct data. In the wrong file. Sue me.

--- js/scenes/About/AboutContainer.js
+++ js/scenes/About/AboutContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FlatList, Text, View } from "react-native"; // TODO: will prolly need to move to About along with the view code
 import PropTypes from "prop-types";
 
 import About from "./About";
@@ -6,11 +7,33 @@ import About from "./About";
 class AboutContainer extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      data: [] // future home of Code of Conduct data
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://r10app-95fea.firebaseio.com/code_of_conduct.json")
+      .then(res => res.json())
+      .then(data => this.setState({ data }))
+      .catch(err => console.log(err));
   }
 
   render() {
-    return <About />;
+    console.log(this.state.data); // log Code of Conduct data to the debugger
+
+    return (
+      <View>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <Text>{`${item.title} ${item.description}`}</Text>
+          )}
+          keyExtractor={item => item.title}
+        />
+      </View>
+    );
+    // <About />; // TODO: restore to original after refactoring view code away
   }
 }
 
