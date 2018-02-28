d4ea50e879ba67a90095004acb56a9802a074e05
Late Worm <travis.rempel@gmail.com>
2018-02-28 10:02:52 -0800

    Set up Speakers reducer.

--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -6,13 +6,13 @@ import AboutReducer from "./modules/about";
 // import FavesReducer from './modules/faves';
 import ScheduleReducer from "./modules/schedule";
 import SessionsReducer from "./modules/sessions";
-// import SpeakersReducer from './modules/speakers';
+import SpeakersReducer from "./modules/speakers";
 
 export default combineReducers({
   navigation: NavigationReducer,
   about: AboutReducer,
   // faves: FavesReducer,
   schedule: ScheduleReducer,
-  sessions: SessionsReducer
-  // speakers: SpeakersReducer,
+  sessions: SessionsReducer,
+  speakers: SpeakersReducer
 });
