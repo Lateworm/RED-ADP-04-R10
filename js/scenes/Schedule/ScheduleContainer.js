72eada2e06a953e5f4d52d99e80671944c26d971
Late Worm <travis.rempel@gmail.com>
2018-02-28 09:54:50 -0800

    In Schedule Container, fetch data and set up title bar.

--- js/scenes/Schedule/ScheduleContainer.js
+++ js/scenes/Schedule/ScheduleContainer.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
+import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 
+// import Redux components
+import { connect } from "react-redux";
+import { fetchSessionsData } from "../../redux/modules/sessions";
+
+// import presentation components
 import Schedule from "./Schedule";
 
-class ScheduleContainer extends Component {
-  constructor() {
-    super();
-    this.state = {};
+class SessionsContainer extends Component {
+  static route = {
+    navigationBar: {
+      title: "Schedule"
+    }
+  };
+
+  componentDidMount() {
+    this.props.dispatch(fetchSessionsData());
   }
 
   render() {
-    return <Schedule />;
+    const { sessionsData, isLoading } = this.props;
+    return <Schedule data={sessionsData} isLoading={isLoading} />;
   }
 }
 
-export default ScheduleContainer;
+const mapStateToProps = state => {
+  return {
+    sessionsData: state.sessions.sessionsData,
+    isLoading: state.sessions.isLoading
+  };
+};
+
+export default connect(mapStateToProps)(SessionsContainer);
