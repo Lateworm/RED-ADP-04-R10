a196766b4ec37e48367f24ee304e438dd12fffc4
Late Worm <travis.rempel@gmail.com>
2018-03-01 15:27:11 -0800

    Add content to Sessions component.

--- js/scenes/Sessions/Sessions.js
+++ js/scenes/Sessions/Sessions.js
@@ -1,17 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import { Text, View, ScrollView } from "react-native";
 
-const Sessions = ({ data }) => (
-	<View>
-		<Text>Sessions Scene.</Text>
-	</View>
+// import presentation components
+import { styles } from "./styles";
+import { colors } from "../../config/styles.js";
+
+import moment from "moment";
+import { goToSpeaker } from "../../navigation/NavigationHelper";
+
+const Sessions = ({ session }) => (
+	<ScrollView>
+		<View>
+			<View>
+				<Text>{session.location}</Text>
+				{console.log(session)}
+			</View>
+			<View>
+				<Text>{session.title}</Text>
+				<Text>{moment.unix(session.start_time).format("LT")}</Text>
+				<Text>{session.description}</Text>
+			</View>
+		</View>
+	</ScrollView>
 );
 
-Sessions.propTypes = {
-	data: PropTypes.array.isRequired
-};
+// Sessions.propTypes = {
+// 	session: PropTypes.object.isRequired
+// };
 
 export default Sessions;
