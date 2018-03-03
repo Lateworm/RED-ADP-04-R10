84b4b0102bef48cc8f47bebfed16667207ba8b25
Late Worm <travis.rempel@gmail.com>
2018-03-03 11:23:59 -0800

    Add navbar title for Faves scene.

--- js/scenes/Faves/FavesContainer.js
+++ js/scenes/Faves/FavesContainer.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+// import Redux components
+import { connect } from "react-redux";
+import { fetchConductData } from "../../redux/modules/about";
+
+// import presentation components
 import Faves from "./Faves";
 
 class FavesContainer extends Component {
+	static route = {
+		navigationBar: {
+			title: "Favourites"
+		}
+	};
+
 	constructor() {
 		super();
 		this.state = {};
