f45f0d22132962ad89c9f62f3f4458fd99592c5c
Late Worm <travis.rempel@gmail.com>
2018-03-04 14:17:56 -0800

    Remove Schedule reducer, create Speaker reducer to fetch one speaker.

---
+++ js/redux/modules/speaker.js
@@ -0,0 +1,77 @@
+const SPEAKER_GET_LOADING = "SPEAKERS_GET_LOADING";
+const SPEAKER_GET_DATA = "SPEAKERS_GET_DATA";
+const SPEAKER_GET_ERROR = "SPEAKERS_GET_ERROR";
+
+// Action creators
+
+export const speakerGetLoading = () => ({
+	type: SPEAKER_GET_LOADING
+});
+
+export function speakerGet(speakersData) {
+	return {
+		type: SPEAKER_GET_DATA,
+		payload: speakersData
+	};
+}
+
+export const speakerGetError = err => ({
+	type: SPEAKER_GET_ERROR,
+	payload: err
+});
+
+// State
+
+const initialState = {
+	isLoading: false,
+	speakerData: [],
+	errorMessage: ""
+};
+
+// Reducers
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+	case SPEAKER_GET_LOADING:
+		return {
+			...state,
+			isLoading: true
+		};
+	case SPEAKER_GET_DATA:
+		return {
+			...state,
+			speakersData: action.payload,
+			isLoading: false
+		};
+	case SPEAKER_GET_ERROR:
+		return {
+			...state,
+			errorMessage: action.error,
+			isLoading: false
+		};
+	default:
+		return state;
+	}
+};
+
+// async action creator
+
+export const fetchSpeakerData = speakerID => {
+	return dispatch => {
+		dispatch(speakerGetLoading());
+
+		// sample endpoint from slides:
+		// https://r10app-95fea.firebaseio.com/speakers.json?orderBy="speaker_id"&equalTo="-KZ2o1CzG5GOfmURNSUB"
+
+		fetch(
+			`https://r10app-95fea.firebaseio.com/speakers.json?orderBy="speaker_id"&equalTo="${speakerID}"`
+		)
+			.then(resp => resp.json())
+			.then(data => {
+				dispatch(speakerGet(data));
+			})
+			.catch(err => {
+				dispatch(speakerGetError(err));
+			});
+	};
+};
--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -4,15 +4,15 @@ import { NavigationReducer } from "@expo/ex-navigation";
 
 import AboutReducer from "./modules/about";
 // import FavesReducer from './modules/faves';
-import ScheduleReducer from "./modules/schedule";
 import SessionsReducer from "./modules/sessions";
 import SpeakersReducer from "./modules/speakers";
+import SpeakerReducer from "./modules/speaker";
 
 export default combineReducers({
 	navigation: NavigationReducer,
 	about: AboutReducer,
 	// faves: FavesReducer,
-	schedule: ScheduleReducer,
 	sessions: SessionsReducer,
-	speakers: SpeakersReducer
+	speakers: SpeakersReducer,
+	speaker: SpeakerReducer
 });
