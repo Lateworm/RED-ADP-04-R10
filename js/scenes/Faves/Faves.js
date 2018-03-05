f9509a9e0a7542b68b0b41df3727b736065860fd
Late Worm <travis.rempel@gmail.com>
2018-03-05 11:50:06 -0800

    Copy view code from Schedule to Faves.

--- js/scenes/Faves/Faves.js
+++ js/scenes/Faves/Faves.js
@@ -1,12 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Text, View } from "react-native";
+import moment from "moment";
+import { goToSession } from "../../navigation/NavigationHelper";
+
+// import presentation components
+import { SectionList, Text, TouchableHighlight, View } from "react-native";
 import { styles } from "./styles";
+import { colors } from "../../config/styles.js";
 
 const Faves = () => (
 	<View>
-		<Text>Faves Scene. My favourite scene of this whole project.</Text>
+		<SectionList
+			renderItem={({ item }) => {
+				return (
+					<TouchableHighlight
+						underlayColor={colors.lightGrey}
+						onPress={() => goToSession("schedule", item)}
+					>
+						<View>
+							<View>
+								<Text style={styles.h2}>{item.title}</Text>
+								<Text style={styles.h3}>{item.location}</Text>
+							</View>
+						</View>
+					</TouchableHighlight>
+				);
+			}}
+			keyExtractor={(item, index) => index}
+			ItemSeparatorComponent={() => {
+				return <View style={styles.hr} />;
+			}}
+			renderSectionHeader={({ section }) => (
+				<Text style={styles.h1}>{moment.unix(section.title).format("LT")}</Text>
+			)}
+			sections={sessions}
+		/>
 	</View>
 );
 
